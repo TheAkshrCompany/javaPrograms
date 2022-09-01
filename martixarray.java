5fba3be4ae832c12e238396b47701be98ea7ac3e
TheAkshrCompany <50172501+TheAkshrCompany@users.noreply.github.com>
2022-09-01 21:35:09 +0530

    Update martixarray.java

--- martixarray.java
+++ martixarray.java
@@ -6,20 +6,24 @@ public class martixarray {
         Scanner readeScanner = new Scanner(System.in);
         int largestvalue =0,smallestvalue=0;
         for (int i = 0; i < values.length; i++) {
+            
             for (int j = 0; j < values.length; j++) {
                 System.out.print("Enter the mark of " +(i+1)+" branch");
-                values[i][j]=readeScanner.nextInt();
+                                    smallestvalue= values[i][j];
 
+                values[i][j]=readeScanner.nextInt();
+                
+                    smallestvalue= values[0][0];
+
+            
                 if (largestvalue<= values[i][j]) {
                    largestvalue= values[i][j];
                 }
-                else{
-                    smallestvalue= values[i][j];
-
-                }
+               
                 if (smallestvalue>= values[i][j]) {
                     smallestvalue= values[i][j];
                  }
+                 
             }
         }
         System.out.println(largestvalue+" is the largest value");
